7f1e31c347132ea812aca1891a428dbea6f686e9
blt <blt@HP-BLT>
2025-10-05 21:40:30 +0600

    add !

--- Calculator-git/Program.cs
+++ Calculator-git/Program.cs
@@ -1,9 +1,9 @@
 ﻿Console.WriteLine("Calculator");
 
 Console.WriteLine("enter your first number:");
-double a = double.Parse(Console.ReadLine());
+double a = double.Parse(Console.ReadLine()!);
 
 Console.WriteLine("enter your second number:");
-double b = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine()!);
 
 Console.WriteLine($"Answer: {a + b}");
