05bd1cbfae51447f86610a898ea80b3d45699fce
blt <blt@HP-BLT>
2025-10-05 21:32:31 +0600

    Добавьте файлы проекта.

---
+++ Calculator-git/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Calculator");
+
+Console.WriteLine("enter your first number:");
+double a = double.Parse(Console.ReadLine());
+
+Console.WriteLine("enter your second number:");
+double b = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"Answer: {a + b}");
